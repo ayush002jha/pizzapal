323086aee06f5f7adec6ebee71c57e30e5a6e148
ayush002jha <ayush9494jha@outlook.com>
2024-03-17 01:44:44 +0530

    FlatList for multiple scrollable items

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,43 +1,15 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
-import products from '@/assets/data/products';
-import Colors from '@/src/constants/Colors';
+import { StyleSheet, Text, View, Image, FlatList } from "react-native";
+import products from "@/assets/data/products";
+import ProductListItem from "@/src/components/ProductListItem";
 
-const product = products[0];
-
-
-export default function TabOneScreen() {
+export default function MenuScreen() {
   return (
-    
-    <View style={styles.container}>
-      <Image source={{uri: product.image}} style={styles.image}/>
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
-    </View>
+    <FlatList
+      data={products}
+      renderItem={({ item }) => <ProductListItem product={item} />}
+      numColumns={2}
+      columnWrapperStyle={{ gap: 10 }} // This is being used to have spacing between columns
+      contentContainerStyle={{ gap: 10, padding: 10 }} // spacing between rows (GAP) and Spacing all around container (PADDING)
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "white",
-    padding: 10,
-    borderRadius: 10
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: "600",
-    marginVertical:10
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-  price:{
-    color: Colors.light.tint,
-    fontWeight: "bold"
-  },
-  image:{
-    width: "100%", // To make width relative/responsive to the screen size use the %
-    aspectRatio: 1, // Renders the square since height and width is same for aspectRatio 1
-  }
-});
